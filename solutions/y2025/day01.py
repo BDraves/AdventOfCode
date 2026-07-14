f73ddb7f0eb4704c780a48495fbef64d0bdf5e96
brennandraves <dravesbrennan@gmail.com>
2026-07-13 21:46:27 -0700

    day 1 part 1 initial commit

--- solutions/y2025/day01.py
+++ solutions/y2025/day01.py
@@ -7,8 +7,19 @@ class Day01(LineSolution):
     year = 2025
     day = 1
 
+    def perform_turns(self) -> list[int]:
+        results: list[int] = []
+        current_value: int = 0
+        for line in self.input:
+            if line.startswith("L"):
+                current_value -= int(line[1:])
+            elif line.startswith("R"):
+                current_value += int(line[1:])
+            results.append(current_value)
+        return results
+
     def part1(self) -> Any:
-        return None
+        return sum([1 for value in self.perform_turns() if value == 0])
 
     def part2(self) -> Any:
         return None
